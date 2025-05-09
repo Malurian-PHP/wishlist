756632238769087e94bb5953707fa27ad8728975
Gerard Onesi <53016099+Malurian-PHP@users.noreply.github.com>
2025-05-09 06:05:32 +0100

    test user has not products in wishlist

--- tests/Feature/WishlistTest.php
+++ tests/Feature/WishlistTest.php
@@ -29,4 +29,22 @@ class WishlistTest extends TestCase
             'message' => 'Product added to wishlist successfully.',
         ]);
     }
+
+    public function test_user_has_no_products_in_wishlist()
+    {
+        $this->withoutMiddleware();
+
+        $user = \App\Models\User::factory()->create([
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        $response = $this->actingAs($user, 'api')->getJson('/api/wishlist');
+
+        $response->assertStatus(404);
+        $response->assertJson([
+            'success' => false,
+            'message' => 'No products in wishlist.',
+        ]);
+    }
 }
